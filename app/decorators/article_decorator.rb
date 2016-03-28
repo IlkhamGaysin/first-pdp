88547550f132693030f1522fa9e2cfd1e7692c1c
IlkhamGaysin <ilkham.gaysin@flatstack.com>
2016-03-28 20:08:25 +0300

    Fix

--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -1,5 +1,5 @@
 class ArticleDecorator < ApplicationDecorator
-  delegate :id, :title, :description, :author, :new_record?
+  delegate :id, :title, :description, :author, :new_record?, :errors
 
   def author
     I18n.t("app.articles.author_prefix") + object.user.full_name
