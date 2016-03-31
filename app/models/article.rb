6cd3ac7c77da9121b0da28e43a30094086a8386b
IlkhamGaysin <ilkham.gaysin@flatstack.com>
2016-03-31 14:21:33 +0300

    Update seeds and added default per for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,10 @@
 class Article < ActiveRecord::Base
+  paginates_per 10
+
   belongs_to :user
   has_many :comments, dependent: :destroy
+
   validates :title, :description,  presence: true
+
   scope :recent, -> { order(created_at: :desc) }
 end
